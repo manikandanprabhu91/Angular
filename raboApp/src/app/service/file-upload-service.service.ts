e525859f9122824c5047b01abe0181ebe9f255a4
manikandanprabhu91 <manikandanprabhu91@yahoo.com>
2019-06-27 19:57:50 +0530

    Update file-upload-service.service.ts

--- raboApp/src/app/service/file-upload-service.service.ts
+++ raboApp/src/app/service/file-upload-service.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient ,HttpHeaders} from '@angular/common/http';
-import * as serverUrl from '../config/config'
+import * as serverUrl from '../config'
 
 
 @Injectable()
